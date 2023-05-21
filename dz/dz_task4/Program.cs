4dfe033f5274835dbc61ac08ac941912c15ed79a
bryuhin.ivan <bryuhin.ivan@gmail.com>
2023-05-21 19:35:27 +0500

    added solution dz_task4

--- dz/dz_task4/Program.cs
+++ dz/dz_task4/Program.cs
@@ -10,14 +10,14 @@ int number2 = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите третие число: ");
 int number3 = int.Parse(Console.ReadLine()!);
 
-if (number1 > number2)
-{
-    if (number1 > number3)
-    Console.WriteLine("Максимальное: " + number1);
+int max = number1;
 
-else 
-    if (number2 > number3)
-    Console.WriteLine("Максимальное: " + number2);
-    else 
-    Console.WriteLine("Максимальное: " + number3);
+if (number2 > max)
+{
+    max = number2;
 }
+if (number3 > max)
+{
+    max = number3;
+}    
+    Console.WriteLine("Максимальное число: " + max);
