53148d2a9c86a985b01f08a8f85d83aa37ecf607
bryuhin.ivan <bryuhin.ivan@gmail.com>
2023-05-29 23:16:25 +0500

    added solution dz_task19 using methods

--- dz/dz_task19/Program.cs
+++ dz/dz_task19/Program.cs
@@ -4,6 +4,66 @@
 // 12821 -> да
 // 23432 -> да
 
+
+
+
+// Рабочая программа с методами
+
+// Метод FindLastDigit находит последнюю цифру заданного числа
+int FindLastDigit (int AAAAA) // AAAAA - пятизначное число
+{
+    int num = AAAAA % 10;
+    return num;
+}
+
+// Метод NumberToPoint определяет число, которое осталось после отсечения последней цифры
+int NumberToPoint (int BBBB) // BBBB - число после отсечения последней цифры
+{
+    BBBB = BBBB / 10;
+    return BBBB;
+}
+
+// Метод FindPalindrome сравнивает первые две цифры и последние 2 цифры пятизначного числа
+// и определяет является ли заданное число полиндромом.
+void FindPalindrome(int num0, int num4, int num1, int num3)
+{
+if (num0 == num4 && num1 == num3)
+    {
+    Console.WriteLine("Число является палиндромом");
+    }
+else
+    {
+    Console.WriteLine("Число не является палиндромом");
+    }
+}
+
+Console.Clear(); 
+
+Console.Write("Введите пятизначное число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+int num4 = FindLastDigit(num);
+int numA = NumberToPoint(num);
+
+int num3 = FindLastDigit(numA);
+int numB = NumberToPoint(numA);
+
+int num2 = FindLastDigit(numB);
+int numC = NumberToPoint(numB);
+
+int num1 = FindLastDigit(numC);
+int numD = NumberToPoint(numC);
+
+int num0 = FindLastDigit(numD);
+
+Console.WriteLine($"{num} {numA} {numB} {numC} {numD}"); // команда выводит на экран все переменные
+FindPalindrome(num0, num4, num1, num3);
+
+
+
+
+// Рабочая программа без методов
+/*
 Console.Clear();
 
 Console.Write("Введите пятизначное число: ");
@@ -34,6 +94,4 @@ else
     {
     Console.WriteLine("Число не является палиндромом");
     }
-
-
-
+*/
