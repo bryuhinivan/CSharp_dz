ef959a09b089fce59592c7dc56bdba7269052805
bryuhin.ivan <bryuhin.ivan@gmail.com>
2023-05-31 21:13:23 +0500

    new solution task28

--- seminar4_Functions/task28/Program.cs
+++ seminar4_Functions/task28/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿// Задача 28: Напишите программу, которая принимает на вход число N
+// и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+
+Console.Clear();
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int Multipli(int number)
+{
+    int mult = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        mult = mult * i;
+        Console.Write(mult + " ");
+    }
+    return mult;
+}
+
+int num = Prompt("Введите число: ");
+Console.WriteLine($"Произведение всех чисел от 1 до заданного числа: {Multipli(num)}");
