e321a129a7295eb29857f3e769046cf0b8aea449
bryuhin.ivan <bryuhin.ivan@gmail.com>
2023-06-04 22:19:24 +0500

    added solution dz_task36 using method + comments

--- dz/dz_task36/Program.cs
+++ dz/dz_task36/Program.cs
@@ -4,7 +4,7 @@
 // [-4, -6, 89, 6] -> 0
 
 
-int[] GetArray(int size, int minValue, int maxValue)
+int[] GetArray(int size, int minValue, int maxValue) // Метод определяет массив, который приходит из строки 40 
 {
     int[] res = new int[size];
     for (int i = 0; i < size; i++)
@@ -14,14 +14,14 @@ int[] GetArray(int size, int minValue, int maxValue)
     }
     return res;
 }
-void PrintArray(int[] arr)
+void PrintArray(int[] arr) // Метод выводит в терминал весь массив
 {
     for (int i = 0; i < arr.Length; i++)
     {
         Console.Write($"{arr[i]} ");
     }
 }
-void SumOdd(int[] arr)
+void SumOdd(int[] arr) // Метод определяет сумму элементов стоящих на нечётных позициях и выводит сумму в терминал
 {
     int sum = 0;
     for (int i=0; i<arr.Length; i++)
@@ -35,8 +35,7 @@ void SumOdd(int[] arr)
     Console.WriteLine ($"Сумма элементов стоящих на нечётных позициях: {sum} ");
 }
 
-
 Console.Clear();
-int[] randomMassiv = GetArray(4, 1, 11);
+int[] randomMassiv = GetArray(4, 1, 11); // 4 - размер массива, массив от 1 до 10
 PrintArray (randomMassiv);
 SumOdd(randomMassiv);
