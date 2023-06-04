4f386918267e2c281e99f3fdd91255d67e57e19b
bryuhin.ivan <bryuhin.ivan@gmail.com>
2023-06-04 18:11:10 +0500

    added solution dz_task32 using method

---
+++ seminar5/task32/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 32. Напишите программу замена элементов массива: положительные элементы замените 
+// на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+Console.Clear();
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+
+    }
+    return res;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int [] array = GetArray(4, -10, 10);
+PrintArray (array);
+
+// foreach (int el in array)
+// {
+//         el= el*-1;
+// }
+
+for (int i = 0; i<array.Length; i++)
+{
+    array[i]= array[i]*-1;
+}
+
+Console.WriteLine();
+PrintArray (array);
